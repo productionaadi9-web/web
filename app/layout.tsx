c78fa7ea823466352c0989f4753e8c6fe20288cd
amaan287 <amaanmirza287@gmail.com>
2025-10-06 00:55:01 +0530

    Seo added
    Signed-off-by: amaan287 <amaanmirza287@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,33 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Aadi Production",
-  description: "Home grown Studio ",
+  description: "Home grown studio in MP, India — where creativity meets passion.",
+
+  openGraph: {
+    title: "Aadi Production",
+    description: "What started as a passion for creating Instagram reels has grown into a full-blown studio in MP, India.",
+    url: "https://aadiproduction.vercel.app/", 
+    siteName: "Aadi Production",
+    images: [
+      {
+        url: "https://res.cloudinary.com/duax5xong/image/upload/v1759692253/Screenshot_2025-10-06_at_12.53.51_AM_ofzqpt.png", 
+        width: 1200,
+        height: 630,
+        alt: "Aadi Production Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Aadi Production",
+    description: "Home grown studio in MP, India — where creativity meets passion.",
+    images: [
+      "https://res.cloudinary.com/duax5xong/image/upload/v1759692253/Screenshot_2025-10-06_at_12.53.51_AM_ofzqpt.png", 
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -26,8 +52,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable}  antialiased bg-black`}
-      >  <Toaster />
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
+      >
+        <Toaster />
         {children}
       </body>
     </html>
