46537c290a7f5e9db9b90d65377b7b4501df4639
amaan287 <amaanmirza287@gmail.com>
2025-10-05 23:35:56 +0530

    fixing build errors
    Signed-off-by: amaan287 <amaanmirza287@gmail.com>

--- app/api/user/delete/[id]/route.ts
+++ app/api/user/delete/[id]/route.ts
@@ -3,15 +3,23 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-    const { id } = params;
-    try {
-        const deletedUser = await prisma.user.delete({
-            where: { id: id },  // Updated to use string directly as per Prisma schema
-        });
-        return NextResponse.json(deletedUser, { status: 200 });
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: "Failed to delete user" }, { status: 500 });
-    }
+export async function DELETE(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+
+  const { id } = await params;
+  
+  try {
+    const deletedUser = await prisma.user.delete({
+      where: { id: id },
+    });
+    return NextResponse.json(deletedUser, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to delete user" },
+      { status: 500 }
+    );
+  }
 }
